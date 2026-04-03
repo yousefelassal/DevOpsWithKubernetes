cd1c5f542355217e84ee38f4f40b28a348a4bc2c
Yousef Elassal <yousef.elassal@shezlong.com>
2026-04-04 01:17:12 +0200

    solve 1.11

--- log_output/file-reader/index.ts
+++ log_output/file-reader/index.ts
@@ -1,14 +1,42 @@
 const logFilePath = "./files/log.txt";
-const logFile = Bun.file(logFilePath);
+const logFilePathCount = "./files/count.txt";
 
 const server = Bun.serve({
   port: 3003,
   routes: {
-    "/": () => {
+    "/": async () => {
+      // log the request to the console
       console.log(
         `${new Date().toISOString()} - Received request for log file`,
       );
-      return new Response(logFile);
+      
+      // create file objects for the log file and the count log file
+      const logFile = Bun.file(logFilePath);
+      const logFileCount = Bun.file(logFilePathCount);
+
+      // check if files exists
+      const logFileExists = await logFile.exists();
+      const logFileCountExists = await logFileCount.exists();
+
+      // create the content to return in the response
+      let content = "";
+
+      // read the content of the log file if it exists, otherwise return a message indicating that there are no logs yet
+      if (logFileExists) {
+        content += `${await logFile.text()}\n`;
+      } else {
+        content += "No logs yet.\n";
+      }
+
+      // read the content of the count log file if it exists, otherwise return a message indicating that there are no count logs yet
+      if (logFileCountExists) {
+        content += `${await logFileCount.text()}`;
+      } else {
+        content += "No count logs yet.";
+      }
+
+      // return the content of the log file in the response
+      return new Response(content);
     },
   },
 });
--- log_output/ping-pong/index.ts
+++ log_output/ping-pong/index.ts
@@ -1,10 +1,21 @@
 let counter = 0;
 
+const logFilePath = "./files/count.txt";
+
 const server = Bun.serve({
   port: 3004,
   routes: {
-    "/pingpong": () => {
+    "/pingpong": async () => {
+      // log the count to the console
       console.log(`${new Date().toISOString()} - ping received: ${counter}`);
+      // write the count to a file
+      try {
+        const logFile = Bun.file(logFilePath);
+        await Bun.write(logFile, `Ping / Pongs: ${counter}`);
+      } catch (error) {
+        console.error(`Error writing to log file: ${error}`);
+      }
+      // return the count in the response
       return new Response(`pong ${counter++}`);
     },
   },
